7532cace6d747e7dc7b0dc0140c2468f9dbc61e8
ronysoumen <ronysoumen@gmail.com>
2023-03-20 09:37:15 +0530

    Reporter added

--- src/main/java/com/test/runner/CucumberRunner.java
+++ src/main/java/com/test/runner/CucumberRunner.java
@@ -1,8 +1,10 @@
 package com.test.runner;
 
 import org.testng.annotations.AfterClass;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -17,40 +19,22 @@ import io.cucumber.testng.TestNGCucumberRunner;
 		monochrome = true, dryRun = false)*/
 @CucumberOptions(
 	      features = {"src/main/java/com/test/features/TestCucumber.feature"},
-	      glue = {"com.test.stepdef"}/*plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty",
-	    			"json:target/cucumber-reports/CucumberTestReport.json","rerun:target/cucumber-reports/rerun.txt" },
-			monochrome = true, dryRun = false
-	      monochrome = true,
-	      dryRun = false*/
+	      glue = {"com.test.stepdef"},
+	    /*		 plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})*/
+	    	  
+	      plugin = { "pretty", 
+	    		  "json:target/cucumber-reports/cucumber.json",
+	    		  "html:target/cucumber-reports/cucumberreport.html",
+	    		  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+	      )
 	      /*plugin = {
 	            "json:build/cucumber-reports/cucumber.json",
 	            "rerun:build/cucumber-reports/rerun.txt"
-	      }*/)
+	      }*/
 
 
 
-public class CucumberRunner  {
-	private TestNGCucumberRunner testNGCucumberRunner;
-	   
-	@BeforeClass(alwaysRun = true)
-	public void setUpClass() {
-		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-	}
+public class CucumberRunner  extends AbstractTestNGCucumberTests{
 
-
-	@AfterClass(alwaysRun = true)
-	public void testDownClass() {
-		testNGCucumberRunner.finish();
-	}
-	@Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
-	public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
-	   testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
-	}
-
-	@DataProvider
-	public Object[][] scenarios() {
-	   return testNGCucumberRunner.provideScenarios();
-	}
-	
 
 }
